72572d2746b1a8d6b4590a045408d0d932035617
Igor Muntyan <igor.muntyan@gmail.com>
2020-08-17 14:54:58 -0400

    SQS permissions

--- sqs-add-permission.sh
+++ sqs-add-permission.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
+aws sqs --region=us-east-2 remove-permission --cli-input-json file://sqs-remove-permission.json
 aws sqs --region=us-east-2 add-permission --cli-input-json file://sqs-add-permission.json
