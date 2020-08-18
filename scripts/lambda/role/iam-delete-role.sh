069811e80f2929eaf5766a4a4f409ebbae092159
Igor Muntyan <igor.muntyan@gmail.com>
2020-08-18 09:40:08 -0400

    complete delete

--- scripts/lambda/role/iam-delete-role.sh
+++ scripts/lambda/role/iam-delete-role.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
+aws iam detach-role-policy --role-name poc-crossaccount-sqs-role --policy-arn arn:aws:iam::854045450972:policy/poc-crossaccount-sqs-policy
 aws iam delete-role --role-name poc-crossaccount-sqs-role
