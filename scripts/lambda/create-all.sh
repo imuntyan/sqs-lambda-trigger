0698d2bb3493d9b1ef8d50ab4faef644ab9bbbed
Igor Muntyan <igor.muntyan@gmail.com>
2020-08-18 09:45:54 -0400

    complete create

--- scripts/lambda/create-all.sh
+++ scripts/lambda/create-all.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
-
+printf 'Creating IAM policy...\n'
 (cd policy; ./iam-create-policy.sh)
+printf 'Done.\n'
+
+printf 'Creating IAM role...\n'
 (cd role; ./iam-create-role.sh)
+printf 'Done.\n'
+
+printf 'Creating lambda function...\n'
 (cd lambda; ./lambda-create-function.sh)
+sleep 10
+printf 'Done.\n'
+
+printf 'Creating event source mapping...\n'
 (cd esm; ./lambda-create-event-source-mapping.sh)
+printf 'Done.\n'
